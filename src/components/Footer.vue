53ca661f94402a37d701b4093defd82d025cf58b
= <=>
2022-04-03 12:12:38 +0800

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,89 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl';
+
+.footer-wrapper {
+  margin-top: 20px;
+  display: flex;
+
+  .copy-right-wrapper {
+    width: 65%;
+    color: $BorderLight;
+    background: linear-gradient(to right, $Primary, #66b1ff);
+    padding: 16px 0 16px 5vw;
+    font-size: 14px;
+    color: $BorderLight;
+    display: flex;
+    align-items: center;
+
+    .logo {
+      width: 240px;
+      display: block;
+    }
+
+    .split {
+      margin: 0 8px;
+    }
+
+    .link {
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+    .tip-website {
+      padding-top: 8px;
+    }
+  }
+
+  .join-us {
+    background: linear-gradient(to right, $Placeholder, $Info);
+    width: 35%;
+    padding: 16px 5vw 16px 40px;
+    color: #fff;
+
+    .join-list {
+      margin-top: 16px;
+      display: flex;
+
+      .join-item {
+        .iconfont {
+          margin-right: 16px;
+          font-size: 30px;
+          cursor: pointer;
+        }
+
+        .qq {
+          &:hover {
+            color: $Primary;
+          }
+        }
+
+        .wechat {
+          &:hover {
+            color: $Success;
+          }
+        }
+
+        .gitee {
+          &:hover {
+            color: #c71d24;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
